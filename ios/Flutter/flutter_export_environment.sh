74a557df7b2af26fd3b8af5cdddfeca16fa4dbbd
Nguyen Huy <nguyenhuy@MacBook-Pro-cua-Nguyen-3.local>
2023-04-23 22:06:42 +0700

    commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nguyenhuy/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nguyenhuy/Desktop/app_doc_truyen/apptruyen"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/nguyenhuy/Desktop/app_doc_truyen/apptruyen/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/nguyenhuy/Desktop/app_doc_truyen/apptruyen/.dart_tool/package_config.json"
